5a9e6dce999fec26ea3e372900fa66273311f552
Enache Andrei <enache.andrei.george@gmail.com>
2023-06-02 01:02:30 +0300

    Started data processing

--- data_manipulation/xml_parser.py
+++ data_manipulation/xml_parser.py
@@ -1,5 +1,24 @@
+import numpy
 
+# print("m", int((float(1710206951735)-float(1710206951735))/1e6))
+# print("m", int((float(1710227306959)-float(1710206951735))/1e6))
+# print("a", int((float(1710222454664)-float(1710206951735))/1e6))
+# print("a", int((float(1710242840406)-float(1710206951735))/1e6))
+# print("m", int((float(1710246929762)-float(1710206951735))/1e6))
+# print("a", int((float(1710262829420)-float(1710206951735))/1e6))
+# print("m", int((float(1710266918776)-float(1710206951735))/1e6))
+# print(float(1710227306959)-float(1710206951735))
+# print(int((float(1710227306959)-float(1710206951735))/1e6))
 
+# st = numpy.append([5, 6], 2, axis=None)
+# print(st)
+
+a=4
+for i in range(1000):
+    if a>=3:
+        break
+    else:
+        print(i)
 
 if __name__ == "__main__":
     pass

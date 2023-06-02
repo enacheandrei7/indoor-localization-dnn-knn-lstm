a4ac83f753e10547df4f7623d547afa97f866af4
Enache Andrei <enache.andrei.george@gmail.com>
2023-06-02 17:40:45 +0300

    Continued implementation of xml parsing

--- data_manipulation/xml_parser.py
+++ data_manipulation/xml_parser.py
@@ -1,6 +1,8 @@
 import numpy
+import math
+import pandas as pd
 
-# print("m", int((float(1710206951735)-float(1710206951735))/1e6))
+print("m", int((float(1936802415164)-float(1710206951735))/1e6))
 # print("m", int((float(1710227306959)-float(1710206951735))/1e6))
 # print("a", int((float(1710222454664)-float(1710206951735))/1e6))
 # print("a", int((float(1710242840406)-float(1710206951735))/1e6))
@@ -15,10 +17,17 @@ import numpy
 
 a=4
 for i in range(1000):
-    if a>=3:
+    if i>=a:
         break
     else:
         print(i)
+
+print(math.isnan(numpy.NaN))
+
+df = pd.DataFrame()
+df.columns = ['st', 'ax', 'ay']
+
+# TODO: Parse the XML file, save the values from sensors as g,m,v (+maybe wifi), the use a similar algorithm to the other program from the paper (check the max difference between time 0 and time n, iterate thorugh that interval range, for every range map the x,y,z values of each sensor in a dict/list, then create a dataframe to vizualize better the values)
 
 if __name__ == "__main__":
     pass
